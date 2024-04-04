b587959716116af2f158a80a5e30f38c910f020d
Textiili <erkki.tiili@gmail.com>
2024-04-04 14:27:33 +0300

    ManuRepoTest

---
+++ app/src/test/java/com/dogproductinventory/app/ManufacturerRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.dogproductinventory.app;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.dogproductinventory.app.domain.Manufacturer;
+import com.dogproductinventory.app.domain.ManufacturerRepository;
+
+@DataJpaTest
+public class ManufacturerRepositoryTest {
+
+    @Autowired
+    private ManufacturerRepository manufacturerRepository;
+
+    @Test 
+    public void createNewManufacturerTest() {
+        Manufacturer manufacturer = new Manufacturer("Mustin Murkina", "Goottikatu 3", "066 066060");
+    	manufacturerRepository.save(manufacturer);
+    	assertThat(manufacturerRepository.findById(2L).get().getName()).isEqualTo("Mustin Murkina");
+    }    
+
+    @Test
+    public void deleteManufacturerTest() {
+        assertThat(manufacturerRepository.findById(2L)).isNotNull();
+
+        manufacturerRepository.deleteById(2L);
+        assertThat(manufacturerRepository.findById(2L)).isEmpty();
+    }
+}
