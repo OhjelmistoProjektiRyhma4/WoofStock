f3b910d6b10fbc33ebf53de26b4cd222ec73e306
Textiili <erkki.tiili@gmail.com>
2024-04-25 11:54:26 +0300

    removed another cors

--- app/src/main/java/com/dogproductinventory/app/domain/PhoneNumber.java
+++ app/src/main/java/com/dogproductinventory/app/domain/PhoneNumber.java
@@ -7,8 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
-// tttt
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = PhoneNumberValidator.class)
